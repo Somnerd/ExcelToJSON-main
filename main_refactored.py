a336513b3d2356b68e99929c331f14dd181fa41b
Somnerd <nikolasalexandrakis1@gmail.com>
2023-05-25 02:19:55 +0300

    last edge cases fixed

--- main_refactored.py
+++ main_refactored.py
@@ -6,7 +6,7 @@ import os
 import argparse
 import re
 
-class DataProcessor:
+class DataHandler:
     def __init__(self, file_path, data_type):
         
         self.file_path = file_path
@@ -38,44 +38,41 @@ class DataProcessor:
 
 
 def process_file(file_path):
+    if os.path.basename(file_path).startswith('Pre'):
+        data_type = 'rain'
+    else:
+        data_type = 'temp'
     
-    print(f'file_path:{file_path}')
-       
-    
-    data_type = 'rain' if os.path.basename(file_path).startswith('Pre') else 'temp'
-    data_processor = DataProcessor(file_path, data_type)
-    excel_data_df = data_processor.read_excel_file()
-    data_processor.process_data(excel_data_df)
-    return data_processor.get_data()
+    data_handler = DataHandler(file_path, data_type)
+    excel_data_df = data_handler.read_excel_file()
+    data_handler.process_data(excel_data_df)
+    return data_handler.get_data()
 
 def main():
-    parser = argparse.ArgumentParser(description='Process climate data files.')
-    parser.add_argument('--folder', help='Folder path containing the data files')
-    parser.add_argument('--all', action='store_true', help='Process all files in the folder')
-
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--folder', help='Folder location ')
+    
     args = parser.parse_args()
 
     if args.folder:
         folder_path = args.folder
-        print(folder_path)
         files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
     else:
         folder_path = ''
         files = [input("File name:")]
-
-    
-    print(f'{files}')
+        
     for file in files:
-        if not re.match(r'^(Pre|Tmean)_', file):
+        # or
+        if not re.match(r'^(Pre|Tmean)_', file) or not re.search(r'\.xls$', file):
             print(f"Skipping file: {file}. Invalid file name format.")
             continue
-
+            
         file_path = os.path.join(folder_path, file)
         data = process_file(file_path)
-
-        with open(f'{file}.json', 'w') as outfile:
+        file = file.replace('.xls', '')
+        with open(f'{folder_path}/{file}.json', 'w') as outfile:
             json.dump(data, outfile, indent=2, ensure_ascii=False)
-
+        
     print("Finished!")
 
 if __name__ == "__main__":
